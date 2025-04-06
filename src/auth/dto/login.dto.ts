c619f31c7e45d1eaf11407c0e3282873d94cd64e
Wiliyanto <wiliyanto@grip-principle.com>
2025-04-06 14:53:35 +0700

    feat: Add swagger documentation on auth

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,13 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginDTO {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,13 +1,15 @@
 import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class RegisterDTO {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email: string;
-
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   username: string;
-
+  @ApiProperty()
   @IsString()
   @MinLength(8, { message: 'Password must be at least 6 characters' })
   password: string;
