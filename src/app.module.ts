280a0ebce44274061510435da2102ca85cd14f8d
Wiliyanto <wiliyanto@grip-principle.com>
2025-04-06 16:20:52 +0700

    chore: connect mongo using env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,22 @@ import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
-import { ChatGateway } from './chat/chat.gateway';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     AuthModule,
     ProfileModule,
-    MongooseModule.forRoot('mongodb://localhost'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGO_URI') ||
+          'mongodb://localhost:27017/test',
+      }),
+      inject: [ConfigService],
+    }),
     UserModule,
     ConfigModule.forRoot(),
     ChatModule,
