86d21a683d099e571d41cf9359be52e742bf637b
Wiliyanto <wiliyanto@grip-principle.com>
2025-04-06 11:03:48 +0700

    feat: Protect websocket on connect instead of use guard and implement send private message by recipient id

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -5,26 +5,56 @@ import {
   WebSocketServer,
   MessageBody,
   ConnectedSocket,
+  WsException,
+  OnGatewayConnection,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { WsJwtGuard } from './guard/chat.guards';
-import { CustomSocket } from './interfaces/chatSocket.interface';
+import { CustomSocket, JWTPayload } from './interfaces/chatSocket.interface';
+import { ConfigService } from '@nestjs/config';
+import * as jwt from 'jsonwebtoken';
+
 @UseGuards(WsJwtGuard)
 @WebSocketGateway(5000)
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('sendMessage')
-  handleMessage(
-    @MessageBody() data: { message: string; recipient: string },
+  constructor(private configService: ConfigService) {}
+
+  async handleConnection(client: CustomSocket) {
+    const token = client.handshake?.headers?.authorization?.split(' ')[1];
+    const secret = this.configService.get<string>('JWT_SECRET');
+
+    if (!token || !secret) {
+      client.disconnect();
+      return;
+    }
+
+    try {
+      const payload = jwt.verify(token, secret) as JWTPayload;
+      client.user = payload;
+
+      await client.join(client.user.userId);
+    } catch (err) {
+      console.log('Invalid token');
+      client.disconnect();
+    }
+  }
+
+  @SubscribeMessage('sendPrivateMessage')
+  handlePrivateMessage(
+    @MessageBody() data: { recipient: string; message: string },
     @ConnectedSocket() client: CustomSocket,
   ) {
-    const user = client.user;
+    if (!client.user) throw new WsException('Unauthorized');
+    const senderId = client.user.userId;
 
-    this.server.emit('receiveMessage', {
-      sender: user?.userId,
+    const payload = {
+      senderId,
       message: data.message,
-    });
+    };
+
+    this.server.to(data.recipient).emit('receivePrivateMessage', payload);
   }
 }
--- src/chat/interfaces/chatSocket.interface.ts
+++ src/chat/interfaces/chatSocket.interface.ts
@@ -1,8 +1,10 @@
 import { Socket } from 'socket.io';
 
+export type JWTPayload = {
+  username: string;
+  userId: string;
+};
+
 export interface CustomSocket extends Socket {
-  user?: {
-    username: string;
-    userId: string;
-  };
+  user?: JWTPayload;
 }
